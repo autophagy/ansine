365b8e4ab8354b96bed9a9ebe13b3616206145d6
Mika Naylor <mail@autophagy.io>
2023-02-13 21:27:46 +0100

    Better test for proc_stat, assume whole file is being parsed

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::str;
 use std::time::Duration;
 
 use nom::{
-    bytes::complete::tag,
+    bytes::complete::{tag, take_until},
     character::complete::{digit1, multispace0},
     combinator::{map, map_res},
     error::ParseError,
     number::complete::double,
-    sequence::{delimited, tuple},
+    sequence::{delimited, preceded, tuple},
     IResult,
 };
 
@@ -48,8 +48,8 @@ fn parse_f64(i: &str) -> IResult<&str, f64> {
 }
 
 fn parse_stat(i: &str) -> IResult<&str, Stat> {
-    let (i, _) = tag("cpu")(i)?;
-    let (i, (user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice)) = tuple((
+    let (i, _) = take_until("cpu ")(i)?;
+    let parser = tuple((
         parse_usize,
         parse_usize,
         parse_usize,
@@ -60,7 +60,10 @@ fn parse_stat(i: &str) -> IResult<&str, Stat> {
         parse_usize,
         parse_usize,
         parse_usize,
-    ))(i)?;
+    ));
+
+    let (i, (user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice)) =
+        preceded(tag("cpu "), parser)(i)?;
     Ok((
         i,
         Stat {
@@ -87,18 +90,27 @@ fn main() {}
 
 #[test]
 fn parse_proc_stat() {
-    let proc_stat = "cpu  8934555 4605 1022996 89784995 91669 0 89602 0 0 0";
+    let proc_stat = "cpu  9701702 6293 1291945 119400172 120770 0 120369 0 0 0
+cpu0 1209513 784 169115 14910230 15511 0 34945 0 0 0
+cpu1 1209721 776 161430 14923348 15558 0 15489 0 0 0
+cpu2 1217037 764 158973 14942082 15003 0 13775 0 0 0
+cpu3 1307743 793 163042 14833254 14664 0 4384 0 0 0
+cpu4 1205766 755 153402 14966185 14950 0 8169 0 0 0
+cpu5 1215377 766 152806 14948197 15296 0 13306 0 0 0
+cpu6 1218276 832 158639 14917222 14966 0 4001 0 0 0
+cpu7 1118264 821 174536 14959651 14820 0 26297 0 0 0
+";
     let (_, stat) = parse_stat(proc_stat).unwrap();
     assert_eq!(
         stat,
         Stat {
-            user: 8934555,
-            nice: 4605,
-            system: 1022996,
-            idle: 89784995,
-            iowait: 91669,
+            user: 9701702,
+            nice: 6293,
+            system: 1291945,
+            idle: 119400172,
+            iowait: 120770,
             irq: 0,
-            softirq: 89602,
+            softirq: 120369,
             steal: 0,
             guest: 0,
             guest_nice: 0
