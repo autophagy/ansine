ead33481f5edcd2879483bbc70b7565e9bf98a2f
Mika Naylor <mail@autophagy.io>
2023-02-18 20:32:34 +0100

    Add nixos module

--- flake.nix
+++ flake.nix
@@ -7,26 +7,40 @@
   };
 
   outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = naersk.lib.${system};
-      in
-      {
-        packages = rec {
-          ansine = naersk-lib.buildPackage {
-            root = ./.;
-            doCheck = true;
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          naersk-lib = pkgs.callPackage naersk { };
+        in
+        rec {
+          # `nix build`
+          packages = {
+            ansine = naersk-lib.buildPackage {
+              root = ./.;
+              doCheck = true;
+            };
+            default = packages.ansine;
           };
+
+          devShell = with pkgs; mkShell {
+            buildInputs = [ cargo rustc rustfmt rustPackages.clippy ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+
+          formatter = pkgs.nixpkgs-fmt;
+        }) // {
+      overlays = {
+        ansine = final: prev: { inherit (self.packages.${final.system}) ansine; };
+        default = self.overlays.ansine;
+      };
+
+      nixosModules = {
+        ansine = { pkgs, ... }: {
+          nixpkgs.overlays = [ self.overlays.default ];
+          imports = [ ./module.nix ];
         };
-
-        defaultPackage = self.packages.${system}.ansine;
-
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
-
-        formatter = pkgs.nixpkgs-fmt;
-      });
+        default = self.nixosModules.ansine;
+      };
+    };
 }
