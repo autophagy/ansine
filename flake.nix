3851e814fd4116875e03c67d32f58bcaad1a0876
Mika Naylor <mail@autophagy.io>
2023-02-18 14:49:14 +0100

    Reintroduce naersk
    Turns out I just needed to update the flake inputs. I'm stupid

--- flake.nix
+++ flake.nix
@@ -1,26 +1,32 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    naersk.url = "github:nix-community/naersk/master";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, naersk }:
     utils.lib.eachDefaultSystem (system:
-    let
+      let
         pkgs = import nixpkgs { inherit system; };
+        naersk-lib = naersk.lib.${system};
       in
       {
-        packages.ansine = pkgs.rustPlatform.buildRustPackage {
-          pname = "ansine";
-          version = "0.1.0";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
+        packages = rec {
+          ansine = naersk-lib.buildPackage {
+            root = ./.;
+            doCheck = true;
+          };
         };
 
         defaultPackage = self.packages.${system}.ansine;
+
         devShell = with pkgs; mkShell {
           buildInputs = [ cargo rustc rustfmt rustPackages.clippy ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
+
+        formatter = pkgs.nixpkgs-fmt;
       });
 }
